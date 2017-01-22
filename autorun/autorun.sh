ad3fe2f3dc3a3122d5fcb9c960c6b5f938d104b5
Andreas Kopp <andreasrkopp@gmail.com>
2017-01-22 10:18:51 +0100

    neuer controller

--- autorun/autorun.sh
+++ autorun/autorun.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 #  Dieses script führt alle Python und Bash-Scripte im selben Ordner
-#  aus, die mit einer Ziffer beginnen und auf .py oder .sh enden.
-#
+#  aus, die mit einer Ziffer beginnen und auf .py oder .sh und auch alle Scratch Programme
+#  mit der Endung sb aus.
+
 #  Um ein Script zu deaktivieren, muss lediglich die führende
 #  Zahl entfernt werden.
 
@@ -13,5 +14,7 @@ for i in [0-9]*.{py,sh}; do
 		python "$i" > $i.log &
 	elif [ -e $i ] && [ ${i##*.} == 'sh' ]; then
 		./$i > $i.log &
+    elfif [ -e $i ] && [ ${i##*.} == 'sb' ]; then
+        scratch presentation "$i"  > $i.log &
 	fi
 done

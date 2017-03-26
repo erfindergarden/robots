51418a40e3f742f476bf5e5f1a31d6a8c38d4575
Joseph Paul <mail@jsph.pl>
2017-03-26 16:06:53 +0200

    Add gpiozero code

---
+++ code_gpiozero/40-drive-obstacle.py
@@ -0,0 +1,23 @@
+import gpiozero
+import time
+
+robot = gpiozero.CamJamKitRobot()
+sensor = gpiozero.DistanceSensor(18, 17)
+
+speed = 0.2
+min_distance = 25 # cm
+
+def avoid():
+	print("Avoiding obstacle.")
+	robot.left(speed)
+	time.sleep(0.2)
+
+try:
+	while True:
+		if sensor.distance < min_distance:
+			avoid()
+		else:
+			robot.forward(speed)
+		time.sleep(0.1)
+except KeyboardInterrupt:
+	robot.stop()
